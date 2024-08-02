92f1b7af0cb674f3fff17e0c4ce88a1947e0a78d
xiongke <917634854@qq.com>
2024-08-02 15:20:01 +0800

    feat: :sparkles: 2024.8.2
    Signed-off-by: xiongke <917634854@qq.com>

--- demo-leetcode/src/main/java/com/xiaogong/leetcode/动态规划/V198_打家劫舍.java
+++ demo-leetcode/src/main/java/com/xiaogong/leetcode/动态规划/V198_打家劫舍.java
@@ -13,6 +13,9 @@ public class V198_打家劫舍 {
     }
 
     private static int rob(int[] nums) {
+        if (nums.length==1){
+            return nums[0];
+        }
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
